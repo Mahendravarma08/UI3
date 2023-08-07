b36ee302be21999ab7bd3f0528561438ae3d6758
Mahendra Varma <mahendrabuddharaju3886@gmail.com>
2023-08-07 22:45:16 +0530

    Initial Setup

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginform!: FormGroup;
+  ngOnInit(): void {
+    this.loginform = new FormGroup({
+      'email': new FormControl(Validators.required),
+      'password': new FormControl(Validators.required)
+    })
+  }
+
+}
